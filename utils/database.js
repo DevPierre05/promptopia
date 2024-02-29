5d58eaa56f54d8a943d829200510ad738120497f
DevPierre05 <pierrosei2009@yahoo.co.uk>
2024-02-29 21:15:09 +0000

    database changes

--- utils/database.js
+++ utils/database.js
@@ -12,7 +12,9 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+    });
 
     isConnected = true;
 
